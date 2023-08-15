6a3782cca17c657b3c7a13b90c37e9c9df737bb7
Gabriel Dickman <gabrieldickman33@gmail.com>
2023-08-15 12:58:59 -0400

    Inicializacao
    Inicializado projeto e router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/company' exact element={<Company />}></Route>
+        <Route path='/contact' exact element={<Contact />}></Route>
+        <Route path='/newproject' exact element={<NewProject />}></Route>
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,9 @@
+function NewProject(){
+  return (
+    <div>
+      <h1>NOVO PROJETO</h1>
+    </div>
+  )
+}
+
+export default NewProject
